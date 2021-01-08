ed5af386a42ad90b1c3d5d241e796588adba54c2
Phillip Plummer <pplummer@ciena.com>
2021-01-08 14:23:13 -0800

    BPCC-6025: Add JIRA link to tickets in PR title

--- scripts/central-jira-links.user.js
+++ scripts/central-jira-links.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Central JIRA Links
 // @namespace    https://github.com/ciena-it-central
-// @version      0.2
+// @version      0.3
 // @description  Replace BPCC-X text in Github PRs with links
 // @author       pplummer@ciena.com
 // @match        https://github.com/ciena-it-central/*/pull/*
 // @grant        none
 // ==/UserScript==
+
+function addJiraLink (el) {
+  el.innerHTML = el.innerHTML.replaceAll(
+    /(BPCC-[0-9]+)/g, '<a href="https://agile-jira.ciena.com/browse/$1" target="_blank">$1</a>')
+}
 
 (function() {
   'use strict';
@@ -16,7 +21,9 @@
     if (el.childNodes.length > 1) {
       continue // only concerned with leaf nodes, which contain only #text node
     }
-    el.innerHTML = el.innerHTML.replaceAll(
-      /(BPCC-[0-9]+)/g, '<a href="https://agile-jira.ciena.com/browse/$1" target="_blank">$1</a>')
+    addJiraLink(el)
   }
+
+  const prTitle = document.querySelector('.js-issue-title')
+  addJiraLink(prTitle)
 })();
